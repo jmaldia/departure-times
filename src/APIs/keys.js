9c0bd17d1ff6682bbc3f53fa28c53f0f6119ea28
Jon Maldia <jon@maldia.net>
2018-11-04 17:30:33 -0500

    Add Google Map Component

---
+++ src/APIs/keys.js
@@ -0,0 +1,5 @@
+export const keys = {
+    googleMaps: {
+        APIkey: 'AIzaSyD2Ow6iH0zHR4v1KFvp96IS2owFoYTWta0'
+    }
+}
---
+++ src/Components/GoogleMapComponent.js
@@ -0,0 +1,50 @@
+// global google
+import React, { Component } from 'react'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
+import { keys } from '../APIs/keys';
+import '../App.css'
+
+const MapComponent = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={15}
+    zoom={15}
+    defaultCenter={{ lat: 40.8257712, lng: -74.1074718 }}
+    center={{ lat: 40.8257712, lng: -74.1074718 }}
+  >
+    { 
+        // props.markerResults && props.markerResults.filter(marker => marker.isVisible).map((marker) => (
+        //     <Marker 
+        //         key={`m-${marker.id}`} 
+        //         aria-label="Marker"
+        //         position={{ lat: marker.lat, lng: marker.lng }} 
+        //         onClick={() => props.clickMarker(marker) }
+        //         animation={marker.animation}
+        //     >
+        //         { marker.isOpen && (
+        //             <InfoWindow>
+        //                 <h3>{marker.name}<br />{marker.address}</h3>
+        //             </InfoWindow>
+        //         )}
+        //     </Marker>
+        // ))
+    }
+  </GoogleMap>
+))
+
+class GoogleMapComponent extends Component {
+    
+    render() {
+        return (
+            <MapComponent
+                {...this.props}
+                aria-label="Map"
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&key=${keys.googleMaps.APIkey}`}
+                loadingElement={<div style={{ height: `100%`,  width: `100%` }} />}
+                containerElement={<div style={{ height: `100vh`, width: `100%` }} />}
+                mapElement={<div style={{ height: `100%`, width: `100%` }} />}
+            />
+        )
+    }
+}
+
+export default GoogleMapComponent
